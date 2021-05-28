091f8c2e84b0812e5c3dd8da485ccbc68338b0b4
H. Turgut Uyar <uyar@tekir.org>
2021-05-28 22:00:19 +0300

    remove unnecessary functions

--- chrome/content/options.js
+++ chrome/content/options.js
@@ -6,113 +6,11 @@ var gScreenshoterOptions = {
 			.getService(Components.interfaces.nsIPrefService)
 			.getBranch("extensions.web-screenshot."),
 
-	getPref: function(name, type, def_value) {
-		var v = def_value;
-		try {
-			switch (type) {
-				case "bool" :
-					v = gScreenshoterOptions.pref.getBoolPref(name);
-					break;
-				case "int" :
-					v = gScreenshoterOptions.pref.getIntPref(name);
-					break;
-				case "text" :
-					v = gScreenshoterOptions.pref.getCharPref(name);
-					break;
-			}
-		} catch (e) {
-		}
-		return v;
-	},
-
-	setPref: function(name, type, value) {
-		try {
-			switch (type) {
-				case "bool":
-					gScreenshoterOptions.pref.setBoolPref(name, value);
-					break;
-				case "int":
-					gScreenshoterOptions.pref.setIntPref(name,  value);
-					break;
-				case "text":
-					gScreenshoterOptions.pref.setCharPref(name, value);
-					break;
-			}
-		} catch (e) {
-		}
-	},
-
-	setCheck: function(name, defaultValue) {
-		document.getElementById(name).checked = gScreenshoterOptions.getPref(name, "bool", defaultValue);
-	},
-
-	setRadio: function(name, defaultValue) {
-		var value = gScreenshoterOptions.getPref(name, "text", defaultValue);
-		var rg = document.getElementById(name);
-		var rl = rg.getElementsByTagName('radio');
-		for (var i=0; i<rl.length; i++) {
-			var r = rl[i];
-			if (r.value == value) {
-				rg.selectedIndex = i;
-				break;
-			}
-		}
-	},
-
-	setText: function(name, type, defaultValue) {
-		var value = gScreenshoterOptions.getPref(name, type, defaultValue);
-		document.getElementById(name).value = value;
-	},
-
 	onLoad: function() {
 		gScreenshoterOptions.updateJPGImageQuality(gScreenshoterOptions.pref.getIntPref("JPGImageQuality"));
 
 		var savePath = gScreenshoterOptions.pref.getComplexValue("defaultFolder", Components.interfaces.nsISupportsString);
 		document.getElementById("defaultFolder").value = savePath;
-	},
-
-	updateCheckPref: function(name) {
-		var v = document.getElementById(name).checked;
-		gScreenshoterOptions.setPref(name, "bool", v);
-	},
-
-	updateRadioPref: function(name) {
-		var rg = document.getElementById(name);
-		gScreenshoterOptions.setPref(name, "text", rg.selectedItem.value);
-	},
-
-	updateTextPref: function(name, type) {
-		var v = document.getElementById(name).value;
-		gScreenshoterOptions.setPref(name, type, v);
-	},
-
-	doOk: function() {
-		if (!gScreenshoterOptions.checkSaveFolder())
-			return false;
-
-		gScreenshoterOptions.updateRadioPref("defaultAction");
-		gScreenshoterOptions.updateRadioPref("defaultTarget");
-		gScreenshoterOptions.updateRadioPref("defaultType");
-		gScreenshoterOptions.updateTextPref("JPGImageQuality", "int");
-		Screenshoter.setSaveFolder(document.getElementById("defaultFolder").value);
-		gScreenshoterOptions.updateRadioPref("filenameTemplate");
-		gScreenshoterOptions.updateCheckPref("showInDLManager");
-		gScreenshoterOptions.updateCheckPref("showInContextMenu");
-		gScreenshoterOptions.updateRadioPref("notificationType");
-
-		return true;
-	},
-
-	setDefaultsRadio: function(name, value) {
-		var rg = document.getElementById(name);
-		var rl = rg.getElementsByTagName('radio');
-		for (var i=0; i<rl.length; i++) {
-			var r = rl[i];
-			if (r.value == value) {
-				rg.selectedIndex = i;
-				break;
-			}
-		}
 	},
 
 	changeSaveFolder: function() {
